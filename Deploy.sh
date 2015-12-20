b817c19ee3d2931db3c8c1182878a997f2e6f93c
Zhou Yating <maxblast@163.com>
2015-12-20 22:55:43 +0800

    Removed one line that may cause failure of the progress.
    Removed md5 check which is no more need for this script.

--- Deploy.sh
+++ Deploy.sh
@@ -394,7 +394,6 @@ echo "${BLUE}[Display]${OFF}: Resolution ${BOLD} 3200 x 1800${OFF} found"
 # Patch IOKit.
 #
 echo "${GREEN}[IOKit]${OFF}: Patching IOKit for maximum pixel clock"
-echo "${BLUE}[IOKit]${OFF}: Current IOKit md5 is ${BOLD}${iokit_md5}${OFF}"
 sudo perl -i.bak -pe 's|\xB8\x01\x00\x00\x00\xF6\xC1\x01\x0F\x85|\x33\xC0\x90\x90\x90\x90\x90\x90\x90\xE9|sg' /System/Library/Frameworks/IOKit.framework/Versions/Current/IOKit
 sudo codesign -f -s - /System/Library/Frameworks/IOKit.framework/Versions/Current/IOKit
 fi
